32b17598e01cc490c836baf6bc3db1b41f8e7f07
jinseongFBI <wlstjddydrl5@gmail.com>
2021-08-24 15:32:25 +0900

    controlleredcomponent||uncontrolledcomponent

---
+++ src/components/UncontrolledComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class UncontrolledComponent extends React.Component{
+  inputRef = React.createRef();
+
+  render(){
+    console.log("initial render",this.inputRef);
+    return(
+      <div>
+        <input ref={this.inputRef} />
+        <button onClick={this.click}>전송</button>
+      </div>
+    );
+  }
+  componentDidMount(){
+    console.log('componentDidMount',this.inputRef);
+  }
+  click = () => {
+    console.log(this.inputRef.current.value);
+  };
+}
+export default UncontrolledComponent;
